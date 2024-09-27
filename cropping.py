47bfce1145adf989b6f32615fde86f20b84eca9a
hellocym <13681921379@163.com>
2024-09-27 16:35:20 +0000

    update gitignore

--- cropping.py
+++ cropping.py
@@ -15,12 +15,10 @@ from tqdm import tqdm
 
 cam1_path = '/hy-tmp/七贤岭/417290'
 cam2_path = '/hy-tmp/七贤岭/417334'
-vid_name = os.listdir(cam1_path)[0]
-path = os.path.join(cam1_path, vid_name)
+# path = './行人提取的视频/qx242_231208_071059_071200.mp4'
 
-#loading a YOLO model from disk
-model = YOLO('/root/yolov8x.pt')
-# model = YOLO('yolov8x.pt')
+#loading a YOLO model 
+model = YOLO('yolov8x.pt')
 
 #geting names from classes
 dict_classes = model.model.names
@@ -51,30 +49,28 @@ if scale_percent != 100:
     height = int(height * scale_percent / 100)
     print('[INFO] - Dim Scaled: ', (width, height))
     
-# #-------------------------------------------------------
-# ### Video output ####
-# video_name = 'track_result2.mp4'
-# output_path = "rep_" + video_name
-# tmp_output_path = "tmp_" + output_path
-# VIDEO_CODEC = "MP4V"
+#-------------------------------------------------------
+### Video output ####
+video_name = 'track_result2.mp4'
+output_path = "rep_" + video_name
+tmp_output_path = "tmp_" + output_path
+VIDEO_CODEC = "MP4V"
 
-# output_video = cv2.VideoWriter(tmp_output_path, 
-#                                cv2.VideoWriter_fourcc(*VIDEO_CODEC), 
-#                                fps, (width, height))
-# #-------------------------------------------------------
+output_video = cv2.VideoWriter(tmp_output_path, 
+                               cv2.VideoWriter_fourcc(*VIDEO_CODEC), 
+                               fps, (width, height))
+#-------------------------------------------------------
 
-
+#loading a YOLO model 
+model = YOLO('yolov8x.pt')
 
 
 results = model.track(path, classes=0, persist=True)
-import pickle
-with open(f'results_{vid_name[:-4]}.pkl', 'wb') as f:
-    pickle.dump(results, f)
 
-# for r in results:
-#     frame = r.plot()
-#     # add number of people in the frame, left bottom corner, red color, white text, large font
-#     # cur_cnt = 0 if not r.boxes.id.shape[0]
-#     # cv2.putText(frame, f'People: {cur_cnt}', (10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 10, 25)
-#     output_video.write(frame)
-# output_video.release()
+for r in results:
+    frame = r.plot()
+    # add number of people in the frame, left bottom corner, red color, white text, large font
+    cur_cnt = r.boxes.id.shape[0]
+    cv2.putText(frame, f'People: {cur_cnt}', (10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 6, (0, 0, 255), 10, 25)
+    output_video.write(frame)
+output_video.release()
